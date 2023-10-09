935ec4ae003890d0ca0174bd8149e7c4a5e7bfbf
Lucas Rocha dos Santos <lucasrocha77706@gmail.com>
2023-10-09 11:27:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const timer = () => {
+  let firstDay = new Date(2022, 10, 26, 0, 0, 0, 0);
+  let timeElapsed = Date.now() - firstDay;
+  let daysElapsed = Math.floor(timeElapsed / (1000 * 60 * 60 * 24));
+
+  const daysInYear = 365;
+
+  let yearsElapsed = Math.floor(daysElapsed / daysInYear);
+  let timeDays = daysElapsed - (yearsElapsed * daysInYear);
+  let timeHours = Math.floor((timeElapsed / (1000 * 60 * 60)) % 24);
+  let timeMinutes = Math.floor((timeElapsed / (1000 * 60)) % 60);
+  let timeSeconds = Math.floor((timeElapsed / 1000) % 60);
+
+  document.getElementById("tempo-total").innerHTML = `${yearsElapsed} anos... ${timeDays} dias... ${timeHours} horas... ${timeMinutes} minutos... ${timeSeconds} segundos...`;
+
+  setTimeout(timer, 1000);
+};
+
+timer();
+
+
